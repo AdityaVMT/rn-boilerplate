3fff80fd609037b0f0c024bed0aa8b6e0ee415c8
AdityaVMT <aditya.tarale@appsphere.in>
2022-02-09 22:00:34 +0530

    Add: Svg support

--- assets/constants/icons.js
+++ assets/constants/icons.js
@@ -1,1 +1,19 @@
-export default {};
+import HomeIcon from '../icons/home.svg';
+import NotificationIcon from '../icons/bell.svg';
+import TeamIcon from '../icons/message-circle.svg';
+import UserIcon from '../icons/user.svg';
+import UsersIcon from '../icons/users.svg';
+import CartIcon from '../icons/shopping-cart.svg';
+import LocationIcon from '../icons/map-pin.svg';
+import GoogleSvg from '../icons/google.svg';
+
+export {
+  HomeIcon,
+  NotificationIcon,
+  TeamIcon,
+  UserIcon,
+  UsersIcon,
+  GoogleSvg,
+  CartIcon,
+  LocationIcon,
+};
