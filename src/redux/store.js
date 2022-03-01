60b9d88c62e0eb2437026f6f81c169a4fc88da80
AdityaVMT <aditya.tarale@appsphere.in>
2022-03-01 22:13:21 +0530

    Add: Configured debugging tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,17 @@ import {createStore, applyMiddleware} from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import {persistStore, persistReducer} from 'redux-persist';
 import storage from '@react-native-async-storage/async-storage';
+import {composeWithDevTools} from '@redux-devtools/extension';
 import rootReducer from './reducer';
 import rootSaga from './saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const middleware = [sagaMiddleware];
+
+const composeEnhancers = composeWithDevTools({
+  // Specify name here, actionsDenylist, actionsCreators and other options if needed
+});
 
 /**
  * !Why redux-persist?
@@ -26,7 +31,11 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 /* ---- Redux Configuration ---- */
 
-const store = createStore(persistedReducer, {}, applyMiddleware(...middleware));
+const store = createStore(
+  persistedReducer,
+  {},
+  composeEnhancers(applyMiddleware(...middleware))
+);
 
 const persistor = persistStore(store);
 
